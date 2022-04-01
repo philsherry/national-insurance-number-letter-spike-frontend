912d979f6b9d98a5efdd9a12890387c096f8dbe7
chrisjameswright <chrisjameswright@users.noreply.github.com>
2022-04-01 09:12:32 +0100

    Adding tests for full print model

--- test/viewmodels/PrintModelSpec.scala
+++ test/viewmodels/PrintModelSpec.scala
@@ -19,6 +19,7 @@ package viewmodels
 import base.SpecBase
 import models._
 import pages._
+import uk.gov.hmrc.domain.Nino
 import viewmodels.PrintModel._
 
 import java.time.LocalDate
@@ -27,28 +28,35 @@ class PrintModelSpec extends SpecBase {
 
   "PrintModel" - {
 
+    val currentAddressUk = CurrentAddressUk(addressLine1 = "line 1", None, None, postcode = "AA1 1AA")
+    val currentAddressInternational = CurrentAddressInternational(addressLine1 = "line 1", None, None, country = "France")
+    val previousAddressUk = PreviousAddressUk(addressLine1 = "line 1", None, None, postcode = "AA1 1AA",
+      LocalDate.of(2000, 1, 1), LocalDate.of(2001, 1, 1))
+    val previousAddressInternational = PreviousAddressInternational(addressLine1 = "line 1", None, None, country = "France",
+      LocalDate.of(2002, 1, 1), LocalDate.of(2003, 1, 1))
+    val employerAddress = WhatIsYourEmployersAddress(addressLine1 = "line 1", None, None, postcode = "AA1 1AA")
+    val previousEmployerAddress = PreviousEmployersAddress("line 1", None, None)
+
     "get current address" - {
-      val ukAddress = CurrentAddressUk(addressLine1 = "line 1", None, None, postcode = "AA1 1AA")
-      val intAddress = CurrentAddressInternational(addressLine1 = "line 1", None, None, country = "France")
 
       "must extract a UK address" in {
         val userAnswers = UserAnswers("id")
-          .set(WhatIsYourCurrentAddressUkPage, ukAddress).get
+          .set(WhatIsYourCurrentAddressUkPage, currentAddressUk).get
 
         getCurrentAddress(userAnswers) mustBe Some(List("line 1", "AA1 1AA"))
       }
 
       "must extract an international address" in {
         val userAnswers = UserAnswers("id")
-          .set(WhatIsYourCurrentAddressInternationalPage, intAddress).get
+          .set(WhatIsYourCurrentAddressInternationalPage, currentAddressInternational).get
 
         getCurrentAddress(userAnswers) mustBe Some(List("line 1", "France"))
       }
 
       "must extract a UK address if both present" in {
         val userAnswers = UserAnswers("id")
-          .set(WhatIsYourCurrentAddressUkPage, ukAddress).get
-          .set(WhatIsYourCurrentAddressInternationalPage, intAddress).get
+          .set(WhatIsYourCurrentAddressUkPage, currentAddressUk).get
+          .set(WhatIsYourCurrentAddressInternationalPage, currentAddressInternational).get
 
         getCurrentAddress(userAnswers) mustBe Some(List("line 1", "AA1 1AA"))
       }
@@ -62,10 +70,6 @@ class PrintModelSpec extends SpecBase {
     }
 
     "get previous addresses" - {
-      val ukAddress = PreviousAddressUk(addressLine1 = "line 1", None, None, postcode = "AA1 1AA",
-        LocalDate.of(2000, 1, 1), LocalDate.of(2001, 1, 1))
-      val intAddress = PreviousAddressInternational(addressLine1 = "line 1", None, None, country = "France",
-        LocalDate.of(2002, 1, 1), LocalDate.of(2003, 1, 1))
 
       "must extract no addresses" in {
         val userAnswers = UserAnswers("id")
@@ -75,7 +79,7 @@ class PrintModelSpec extends SpecBase {
 
       "must extract a UK address" in {
         val userAnswers = UserAnswers("id")
-          .set(WhatIsYourPreviousAddressUkPage(Index(0)), ukAddress).get
+          .set(WhatIsYourPreviousAddressUkPage(Index(0)), previousAddressUk).get
 
         val expected = List(
           PreviousAddressPrintModel(List("line 1", "AA1 1AA"), "1 January 2000", "1 January 2001")
@@ -85,7 +89,7 @@ class PrintModelSpec extends SpecBase {
 
       "must extract an international address" in {
         val userAnswers = UserAnswers("id")
-          .set(WhatIsYourPreviousAddressInternationalPage(Index(0)), intAddress).get
+          .set(WhatIsYourPreviousAddressInternationalPage(Index(0)), previousAddressInternational).get
 
         val expected = List(
           PreviousAddressPrintModel(List("line 1", "France"), "1 January 2002", "1 January 2003")
@@ -95,10 +99,10 @@ class PrintModelSpec extends SpecBase {
 
       "must extract multiple UK and international addresses" in {
         val userAnswers = UserAnswers("id")
-          .set(WhatIsYourPreviousAddressUkPage(Index(0)), ukAddress).get
-          .set(WhatIsYourPreviousAddressInternationalPage(Index(1)), intAddress).get
-          .set(WhatIsYourPreviousAddressInternationalPage(Index(2)), intAddress).get
-          .set(WhatIsYourPreviousAddressUkPage(Index(3)), ukAddress).get
+          .set(WhatIsYourPreviousAddressUkPage(Index(0)), previousAddressUk).get
+          .set(WhatIsYourPreviousAddressInternationalPage(Index(1)), previousAddressInternational).get
+          .set(WhatIsYourPreviousAddressInternationalPage(Index(2)), previousAddressInternational).get
+          .set(WhatIsYourPreviousAddressUkPage(Index(3)), previousAddressUk).get
 
         val expected = List(
           PreviousAddressPrintModel(List("line 1", "AA1 1AA"), "1 January 2000", "1 January 2001"),
@@ -112,7 +116,6 @@ class PrintModelSpec extends SpecBase {
     }
 
     "get most recent employer" - {
-      val employerAddress = WhatIsYourEmployersAddress(addressLine1 = "line 1", None, None, postcode = "AA1 1AA")
 
       "must extract employer with a start and end" in {
         val userAnswers = UserAnswers("id")
@@ -156,7 +159,6 @@ class PrintModelSpec extends SpecBase {
     }
 
     "get previous employers" - {
-      val previousEmployerAddress = PreviousEmployersAddress("line 1", None, None)
 
       "must extract no previous employers" in {
         val userAnswers = UserAnswers("id")
@@ -199,6 +201,118 @@ class PrintModelSpec extends SpecBase {
 
     }
 
-    // TODO: Add case for full model
+    "from user answers" - {
+
+      "must extract a full model for a completed user answers with a primary document" in {
+        val previousMarriage = PreviousMarriageOrPartnershipDetails(
+          LocalDate.of(2005, 2, 1),
+          LocalDate.of(2006, 3, 2),
+          "reason"
+        )
+
+        val userAnswers = UserAnswers("id")
+          .set(WhatIsYourNamePage, WhatIsYourName("first", Some("middle"), "last")).get
+          .set(WhatIsYourPreviousNamePage, WhatIsYourPreviousName("prev", Some("prev2"), "prev3")).get
+          .set(WhatIsYourDateOfBirthPage, LocalDate.of(1990, 12, 1)).get
+          .set(WhatIsYourCurrentAddressUkPage, currentAddressUk).get
+          .set(WhatIsYourPreviousAddressUkPage(Index(0)), previousAddressUk).get
+          .set(AreYouReturningFromLivingAbroadPage, false).get
+          .set(WhatIsYourTelephoneNumberPage, "1234567890").get
+          .set(WhatIsYourNationalInsuranceNumberPage, Nino("AA123456A")).get
+          .set(WhenDidYouGetMarriedPage, LocalDate.of(2000, 5, 1)).get
+          .set(PreviousMarriageOrPartnershipDetailsPage, previousMarriage).get
+          .set(HaveYouEverClaimedChildBenefitPage, true).get
+          .set(WhatIsYourChildBenefitNumberPage, "CHB12345678").get
+          .set(WhatOtherUkBenefitsHaveYouReceivedPage, "other benefits").get
+          .set(WhatIsYourEmployersNamePage, "employer name").get
+          .set(WhatIsYourEmployersAddressPage, employerAddress).get
+          .set(WhenDidYouStartWorkingForEmployerPage, LocalDate.of(2001, 2, 28)).get
+          .set(WhatOtherUkBenefitsHaveYouReceivedPage, "other benefits").get
+          .set(WhatIsYourPreviousEmployersNamePage(Index(0)), "emp 1").get
+          .set(WhatIsYourPreviousEmployersAddressPage(Index(0)), previousEmployerAddress).get
+          .set(WhenDidYouStartWorkingForPreviousEmployerPage(Index(0)), LocalDate.of(2013, 3, 2)).get
+          .set(WhenDidYouStopWorkingForPreviousEmployerPage(Index(0)), LocalDate.of(2013, 3, 3)).get
+          .set(WhichPrimaryDocumentPage, PrimaryDocument.Passport).get
+
+        val expected = Some(PrintModel(
+          WhatIsYourName("first", Some("middle"), "last"),
+          Some(WhatIsYourPreviousName("prev", Some("prev2"), "prev3")),
+          "1 December 1990",
+          List("line 1", "AA1 1AA"),
+          List(PreviousAddressPrintModel(List("line 1", "AA1 1AA"), "1 January 2000", "1 January 2001")),
+          returningFromLivingAbroad = false,
+          "1234567890",
+          Some("AA123456A"),
+          Some("1 May 2000"),
+          None,
+          Some(PreviousMarriageOrPartnershipPrintModel("1 February 2005", "2 March 2006", "reason")),
+          claimedChildBenefit = true,
+          Some("CHB12345678"),
+          Some("other benefits"),
+          Some(MostRecentUkEmployerPrintModel("employer name", List("line 1", "AA1 1AA"), "28 February 2001", None)),
+          List(PreviousEmployerPrintModel("emp 1", List("line 1"), "2 March 2013", "3 March 2013")),
+          Some("whichPrimaryDocument.passport"),
+          None
+        ))
+
+        from(userAnswers) mustBe expected
+      }
+
+      "must extract a full model for a completed user answers with secondary documents" in {
+        val previousMarriage = PreviousMarriageOrPartnershipDetails(
+          LocalDate.of(2005, 2, 1),
+          LocalDate.of(2006, 3, 2),
+          "reason"
+        )
+
+        val userAnswers = UserAnswers("id")
+          .set(WhatIsYourNamePage, WhatIsYourName("first", Some("middle"), "last")).get
+          .set(WhatIsYourPreviousNamePage, WhatIsYourPreviousName("prev", Some("prev2"), "prev3")).get
+          .set(WhatIsYourDateOfBirthPage, LocalDate.of(1990, 12, 1)).get
+          .set(WhatIsYourCurrentAddressUkPage, currentAddressUk).get
+          .set(WhatIsYourPreviousAddressUkPage(Index(0)), previousAddressUk).get
+          .set(AreYouReturningFromLivingAbroadPage, false).get
+          .set(WhatIsYourTelephoneNumberPage, "1234567890").get
+          .set(WhatIsYourNationalInsuranceNumberPage, Nino("AA123456A")).get
+          .set(WhenDidYouGetMarriedPage, LocalDate.of(2000, 5, 1)).get
+          .set(PreviousMarriageOrPartnershipDetailsPage, previousMarriage).get
+          .set(HaveYouEverClaimedChildBenefitPage, true).get
+          .set(WhatIsYourChildBenefitNumberPage, "CHB12345678").get
+          .set(WhatOtherUkBenefitsHaveYouReceivedPage, "other benefits").get
+          .set(WhatIsYourEmployersNamePage, "employer name").get
+          .set(WhatIsYourEmployersAddressPage, employerAddress).get
+          .set(WhenDidYouStartWorkingForEmployerPage, LocalDate.of(2001, 2, 28)).get
+          .set(WhatOtherUkBenefitsHaveYouReceivedPage, "other benefits").get
+          .set(WhatIsYourPreviousEmployersNamePage(Index(0)), "emp 1").get
+          .set(WhatIsYourPreviousEmployersAddressPage(Index(0)), previousEmployerAddress).get
+          .set(WhenDidYouStartWorkingForPreviousEmployerPage(Index(0)), LocalDate.of(2013, 3, 2)).get
+          .set(WhenDidYouStopWorkingForPreviousEmployerPage(Index(0)), LocalDate.of(2013, 3, 3)).get
+          .set(WhichAlternativeDocumentsPage, Set[AlternativeDocuments](AlternativeDocuments.AdoptionCertificate, AlternativeDocuments.WorkPermit)).get
+
+        val expected = Some(PrintModel(
+          WhatIsYourName("first", Some("middle"), "last"),
+          Some(WhatIsYourPreviousName("prev", Some("prev2"), "prev3")),
+          "1 December 1990",
+          List("line 1", "AA1 1AA"),
+          List(PreviousAddressPrintModel(List("line 1", "AA1 1AA"), "1 January 2000", "1 January 2001")),
+          returningFromLivingAbroad = false,
+          "1234567890",
+          Some("AA123456A"),
+          Some("1 May 2000"),
+          None,
+          Some(PreviousMarriageOrPartnershipPrintModel("1 February 2005", "2 March 2006", "reason")),
+          claimedChildBenefit = true,
+          Some("CHB12345678"),
+          Some("other benefits"),
+          Some(MostRecentUkEmployerPrintModel("employer name", List("line 1", "AA1 1AA"), "28 February 2001", None)),
+          List(PreviousEmployerPrintModel("emp 1", List("line 1"), "2 March 2013", "3 March 2013")),
+          None,
+          Some(List("whichAlternativeDocuments.adoption-certificate", "whichAlternativeDocuments.work-permit"))
+        ))
+
+        from(userAnswers) mustBe expected
+      }
+
+    }
   }
 }
