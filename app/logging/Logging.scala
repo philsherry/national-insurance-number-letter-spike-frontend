1d073bd97723b24b31fc3dbebfb59e85fd2e9c58
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-05-19 13:31:14 +0100

    Add log message to data required action

---
+++ app/logging/Logging.scala
@@ -0,0 +1,9 @@
+package logging
+
+import org.slf4j.{Logger, LoggerFactory}
+
+trait Logging {
+
+  protected val logger: Logger =
+    LoggerFactory.getLogger("application." + getClass.getCanonicalName)
+}
