1bc3a25da1194c109474ef84f0936b418ba525d6
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2022-07-25 10:24:09 +0100

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "3.17.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "3.22.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.11.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "5.24.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "6.4.0",
     "uk.gov.hmrc"       %% "play-language"                  % "5.3.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.64.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.68.0",
     "uk.gov.hmrc"       %% "domain"                         % "8.1.0-play-28",
     "com.dmanchester"   %% "playfop"                        % "1.0",
     "org.typelevel"     %% "cats-core"                      % "2.3.0",
